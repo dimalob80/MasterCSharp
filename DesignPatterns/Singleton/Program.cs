94e7781696865c6755ff0705aec0ef29b758368a
Dmitry Lobanov <DmitryL@iqmetrix.com>
2022-06-15 09:13:10 -0500

    Singleton

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using Singleton;
+using static System.Console;
+
+
+var instance1 = SingletonClass.GetInstance();
+//instance1.Value = 1;
+
+var instance2 = SingletonClass.GetInstance();
+//instance2.Value = 2;
+
+var thread1 = new Thread(()=>
+{
+    var v1 = SingletonClass.GetInstance();
+    Thread.Sleep(1000);
+    WriteLine($"Value in thread 1: {v1.Value}");
+});
+
+var thread2 = new Thread(() =>
+{
+    var v2 = SingletonClass.GetInstance();
+    WriteLine($"Value in thread 2: {v2.Value}");
+    v2.Value = 100;
+});
+
+thread1.Start();
+thread2.Start();
+
+
+WriteLine($"Outside the threads. HashCodes: {instance1.GetHashCode()} | {instance2.GetHashCode()}");
+WriteLine($"Outside the threads. Values: {instance1.Value} | {instance2.Value}");
+
+ReadKey();
+
---
+++ DesignPatterns/Singleton/SingletonClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton;
+
+internal class SingletonClass
+{
+    // Static members are 'eagerly initialized', that is, 
+    // immediately when class is loaded for the first time.
+    // .NET guarantees thread safety for static initialization
+
+    private static SingletonClass _instance = new SingletonClass(new Random().Next(1, 10));
+
+    private int _value;
+    public int Value { get { return _value; } set { Console.WriteLine($"Setting value to {value}"); _value = value; } }
+
+    private SingletonClass(int value)
+    {
+        Value = value;
+        Console.WriteLine($"Creating instance with value: {Value}");
+    }
+
+    public static SingletonClass GetInstance()
+    {
+        Console.WriteLine($"Checking instance value: {_instance?.Value}");
+        //return _instance = _instance ?? new SingletonClass(new Random().Next(1, 10));
+        return _instance!;
+    }
+}
